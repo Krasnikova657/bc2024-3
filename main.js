72f76a3dc00aacf0d922d6addd4f705cc3061636
Valeriia bc2024-4 <Valeriia.Krasnikova@lnu.edu.ua>
2024-10-23 14:44:35 +0300

    Update

--- main.js
+++ main.js
@@ -2,37 +2,50 @@ const { program } = require('commander');
 const fs = require('fs');
 
 program
-  .requiredOption('-i, --input <path>', 'input file path')
-  .option('-o, --output <path>', 'output file path')
-  .option('-d, --display', 'display result');
+  .requiredOption('-i, --input <path>', 'input file path')  
+  .option('-o, --output <path>', 'output file path')      
+  .option('-d, --display', 'display result');               
 
 program.parse(process.argv);
 
 const options = program.opts();
+
+if (!options.input) {
+  console.error('Please, specify input file');
+  process.exit(1);
+}
 
 if (!fs.existsSync(options.input)) {
   console.error('Cannot find input file');
   process.exit(1);
 }
 
-const data = fs.readFileSync(options.input, 'utf-8');
-const parsedData = JSON.parse(data);
+try {
+  const data = fs.readFileSync(options.input, 'utf-8');
+  const parsedData = JSON.parse(data);
 
-let result = ''; 
-
-if (options.display) {
-  console.log(result);
-}
-
-if (options.output) {
-  fs.writeFileSync(options.output, result);
-}
-
-if (!options.output && !options.display) {
-  console.log('Please specify output or display option');
-}
-if (!options.input) {
-    console.error('Please, specify input file');
-    process.exit(1);
+  const incomes = parsedData.find(item => item.txten === "Income, total");
+  const expenses = parsedData.find(item => item.txten === "Expenses, total");
+  
+  let result = '';
+  
+  if (incomes) {
+    result += `Доходи, усього: ${incomes.value}\n`;
   }
   
+  if (expenses) {
+    result += `Витрати, усього: ${expenses.value}\n`;
+  }
+
+  if (options.display) {
+    console.log(result);
+  }
+
+  if (options.output) {
+    fs.writeFileSync(options.output, result);
+  }
+
+} catch (error) {
+  console.error('Error reading or parsing the input file:', error.message);
+  process.exit(1);
+}
